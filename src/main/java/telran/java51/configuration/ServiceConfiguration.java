e4f07402c2ff6ae905707e3842aa98c38ee0aaf5
mitrodash <mitrodash@10.100.102.45>
2024-02-16 23:27:55 +0200

    First commit

--- src/main/java/telran/java51/configuration/ServiceConfiguration.java
+++ src/main/java/telran/java51/configuration/ServiceConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class ServiceConfiguration {
 
 	@Bean
-	public ModelMapper getModelMapper() {
+    public ModelMapper getModelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration()
 					.setFieldMatchingEnabled(true)
